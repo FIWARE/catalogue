96ee3905b4851b233fa9e85225fa2fbd82840575
Jason Fox <jason.fox@fiware.org>
2021-12-20 12:58:29 +0100

    Align latest

--- push-releases.sh
+++ push-releases.sh
@@ -16,6 +16,16 @@ pushImage () {
 	echo ""
 }
 
+pushLatestImage () {
+	docker pull $1:$3 
+	docker tag $1:$3 $2:latest
+	docker push $2:latest
+	docker rmi $2:latest
+	docker rmi $1:$3
+	echo ""
+}
+
+
 
 pushImageLatest () {
 	docker pull $1:$3 
@@ -33,12 +43,16 @@ findRelease () {
 	X=$(curl -s -H "Authorization: JWT ${TOKEN}"  -H "Content-Type: application/json" https://hub.docker.com/v2/repositories/$1/tags/$3 | jq ".images[0].digest")
 	Y=$(curl -s -H "Authorization: JWT ${TOKEN}"  -H "Content-Type: application/json" https://hub.docker.com/v2/repositories/$2/tags/$3 | jq ".images[0].digest")
 
-
 	if [ "$X" = "$Y" ]; then
 	    echo "Hashes are equal. $1 $3"
 	else
-		#echo push $1 $3
 		pushImage $1 $2 $3
+	fi
+
+	if [ $FIRST = true ]; then
+		echo "Pushing latest"
+		pushLatestImage $1 $2 $3
+		FIRST=false
 	fi
 }
 
@@ -48,6 +62,7 @@ pushAll () {
 	SOURCE=${1}
 	NAME=${2}
 	TAGS=$(curl -s https://hub.docker.com/v2/repositories/${SOURCE}/tags/)
+	FIRST=true
 	for row in $( echo $TAGS | jq -c '.results[]'); do
 	    _jq() {
 	     echo ${row} | jq -r ${1}
