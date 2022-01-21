b2bade7a14aa615221696033cd28d93a07d59ebd
Stefan Wiedemann <wistefan@googlemail.com>
2022-01-21 09:03:26 +0100

    upload all in one

--- .github/container-scan.sh
+++ .github/container-scan.sh
@@ -57,7 +57,9 @@ report_file=$(echo "${image:?}.json" | sed  's/\W/-/g')
 
 trap "scanner_docker_compose down > /dev/null 2>&1" int exit
 
-reports=$(pwd)/reports
+image="${1:?}"
+arrIN=(${image//:/})
+reports=$(pwd)/reports/${arrIN[0]}/${arrIN[1]}
 
 function scan() {
   local image="${1:?}"
