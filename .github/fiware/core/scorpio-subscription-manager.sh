e69f7e1ff0471aa36738145c89a7fad277489c16
Jason Fox <jason-fox@users.noreply.github.com>
2024-04-26 09:13:56 +0200

    Use skopeo for multiarch

--- .github/fiware/core/scorpio-subscription-manager.sh
+++ .github/fiware/core/scorpio-subscription-manager.sh
@@ -17,16 +17,14 @@ function dateDiff {
     echo $(((`date -jf %Y-%m-%d "$CURRENTDATE" +%s` - `date -jf %Y-%m-%d "$1" +%s`)/86400))
 }
 echo "$SOURCE - $VERSION - $(dateDiff $DATE) days old."
+
 function clone {
    echo 'cloning from '"$1 $2"' to '"$3"
-   docker pull -q "$1":"$2"
-   docker tag "$1":"$2" "$3":"$2"
-   docker push -q "$3":"$2"
+   skopeo copy --multi-arch all docker://"$1":"$2"  docker://"$3":"$2"
    
    if ! [ -z "$4" ]; then
         echo 'pushing '"$1 $2"' to latest'
-        docker tag "$1":"$2" "$3":latest
-        docker push -q "$3":latest
+        skopeo copy --multi-arch all docker://"$1":"$2"  docker://"$3":latest
    fi
 }
 
