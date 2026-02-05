6a170657df133ca572e0b3f8f4b70f5ff2bab011
GitHub Action <action@github.com>
2026-02-05 14:05:21 +0100

    Add scripts.

---
+++ .github/fiware/core/kafnus-connect.sh
@@ -0,0 +1,39 @@
+set -e
+
+NAME="core/kafnus/kafnus-connect" 
+SOURCE="telefonicaiot/kafnus-connect"
+DOCKER_TARGET="fiware/kaknus-connect"
+QUAY_TARGET="quay.io/fiware/kafnus-connect"
+
+
+REPOSITORY="$(git rev-parse --show-toplevel)/$NAME" 
+TAGS="$(git -C $REPOSITORY rev-list --tags --max-count=1 )"
+VERSION=$(git -C $REPOSITORY describe --exclude 'FIWARE*' --tags $TAGS )
+DATE=`git -C $REPOSITORY log -1 --format=%ai $VERSION | awk '{print $1}'`
+
+function dateDiff {
+    CURRENTDATE=`date +"%Y-%m-%d"`
+    echo $(((`date -jf %Y-%m-%d "$CURRENTDATE" +%s` - `date -jf %Y-%m-%d "$1" +%s`)/86400))
+}
+echo "$SOURCE - $VERSION - $(dateDiff $DATE) days old."
+
+
+function clone {
+   echo 'cloning from '"$1 $2"' to '"$3"
+   skopeo copy --multi-arch all docker://"$1":"$2"  docker://"$3":"$2"
+   
+   if ! [ -z "$4" ]; then
+        echo 'pushing '"$1 $2"' to latest'
+        skopeo copy --multi-arch all docker://"$1":"$2"  docker://"$3":latest
+   fi
+}
+
+for i in "$@" ; do
+    if [[ $i == "docker" ]]; then
+        clone "$SOURCE" "$VERSION" "$DOCKER_TARGET" true
+    fi
+    if [[ $i == "quay" ]]; then
+        clone "$SOURCE" "$VERSION" "$QUAY_TARGET" true
+    fi
+    echo ""
+done
